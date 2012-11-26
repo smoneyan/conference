3d42fab8c6fd0fc43f8f5058a86036fc9847ca9a
smoneyan@gmail.com <smoneyan@gmail.com>
2012-11-26 21:29:26 +0800

    COMSEJBInterface project removed. COMSEE project added

---
+++ COMSEJB/src/java/coms/ejb/beans/stateless/ComsManagerBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coms.ejb.beans.stateless;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author subu
+ */
+@Stateless
+public class ComsManagerBean implements ComsManagerBeanLocal {
+
+  // Add business logic below. (Right-click in editor and choose
+  // "Insert Code > Add Business Method")
+
+}
