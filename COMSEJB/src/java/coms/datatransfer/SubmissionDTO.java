9ba48d20c80ebed630f8710e6c186b1d5da68ed9
smoneyan@gmail.com <smoneyan@gmail.com>
2012-11-27 18:05:57 +0800

    SubmissionDTO started

---
+++ COMSEJB/src/java/coms/datatransfer/SubmissionDTO.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coms.datatransfer;
+
+/**
+ *
+ * @author subu
+ */
+public class SubmissionDTO {
+  private int submissionId;
+  private ItemTypeDTO itemType;
+  private UserDTO submittedUser;
+  private ScheduleDTO confSchedule;
+  
+  
+ 
+}
